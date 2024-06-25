3c01c46261a12e3719af717c70cc51191fe48105
Alexander Gent <acegent2907@optusnet.com.au>
2024-06-26 09:21:06 +1000

    Add files via upload

--- restoredb.sh
+++ restoredb.sh
@@ -2,21 +2,11 @@
 
 currentuser=$(users | awk '{print $1}')
 
-## Restore KODI Database...
-echo -e '\033[1;33mKODI Media Center... \033[1;34mRestoring database\033[0m'
-rm -f -r /home/kodi/
-cp -f -r ../kodicfg/. /home/
-echo -e '\033[1;33mKODI Media Center... \033[1;34mUpdating user permissions\033[0m'
-chmod -f -R 0777 /home/kodi/
-echo -e '\033[1;33mKODI Media Center... \033[1;34mUpdating user ownership\033[0m'
-chown -R root:root /home/kodi
-
-## Restore USERS Database...
-echo -e '\033[1;33mUser '$currentuser'... \033[1;34mRestoring database\033[0m'
+## Restore User Profile...
+echo -e '\033[1;33mUser '$currentuser'... \033[1;34mRestore User Profile\033[0m'
 rm -f -r /home/$currentuser/
-cp -f -r ../homecfg/. /home/
-rm -f -r /home/$currentuser/.config/pulse
-echo -e '\033[1;33mUser '$currentuser'... \033[1;34mUpdating user permissions\033[0m'
+cp -f -r ../UserProfile/. /home/
+echo -e '\033[1;33mUser '$currentuser'... \033[1;34mUpdating User Permissions\033[0m'
 chmod -f -R 0777 /home/$currentuser/
-echo -e '\033[1;33mUser '$currentuser'... \033[1;34mUpdating user ownership\033[0m'
+echo -e '\033[1;33mUser '$currentuser'... \033[1;34mUpdating User Ownership\033[0m'
 chown -R $currentuser:$currentuser /home/$currentuser
