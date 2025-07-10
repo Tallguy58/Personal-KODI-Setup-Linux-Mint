59cc1c80bec0bd670ca544d8f6b44ace4beaeae4
Alexander Gent <aagent298@gmail.com>
2025-07-10 11:17:55 +1000

    Add files via upload

--- backupdb.sh
+++ backupdb.sh
@@ -5,24 +5,26 @@ currentuser=$(users | awk '{print $1}')
 ## Update Permissions
 chmod -Rf 0777 /home
 
-## Delete User Files
-echo '/home/'$currentuser'/.config/pulse'>/tmp/file.lst
-echo '/home/'$currentuser'/.config/session'>>/tmp/file.lst
+## Create Exceptions List
+echo '/home/'$currentuser'/.bash_history'>/tmp/file.lst
+echo '/home/'$currentuser'/.conkyrc'>>/tmp/file.lst
 echo '/home/'$currentuser'/.sudo_as_admin_successful'>>/tmp/file.lst
-echo '/home/'$currentuser'/.bash_history'>>/tmp/file.lst
-echo '/home/'$currentuser'/.var/app/tv.kodi.Kodi/data/userdata/keymaps/keyboard.xml'>>/tmp/file.lst
 echo '/home/'$currentuser'/.xsession-errors'>>/tmp/file.lst
 echo '/home/'$currentuser'/.xsession-errors.old'>>/tmp/file.lst
-echo '/home/'$currentuser'/Desktop/duc.desktop'>>/tmp/file.lst
-echo '/home/'$currentuser'/Desktop/org.kde.krusader.desktop'>>/tmp/file.lst
-echo '/home/'$currentuser'/Desktop/Zoom.desktop'>>/tmp/file.lst
-echo '/home/'$currentuser'/Desktop/skype_skypeforlinux.desktop'>>/tmp/file.lst
-echo '/home/'$currentuser'/Desktop/Games/gnubg.desktop'>>/tmp/file.lst
-echo '/home/'$currentuser'/Desktop/Games/org.gnome.Mahjongg.desktop'>>/tmp/file.lst
-echo '/home/'$currentuser'/Desktop/Games'>>/tmp/file.lst
+echo '/home/'$currentuser'/.config/autostart'>>/tmp/file.lst
+echo '/home/'$currentuser'/.config/pulse'>>/tmp/file.lst
+echo '/home/'$currentuser'/.config/session'>>/tmp/file.lst
+echo '/home/'$currentuser'/.var/app/tv.kodi.Kodi/data/userdata/keymaps'>>/tmp/file.lst
 echo '/home/'$currentuser'/Desktop'>>/tmp/file.lst
+echo '/home/'$currentuser'/Documents'>>/tmp/file.lst
+echo '/home/'$currentuser'/Downloads'>>/tmp/file.lst
+echo '/home/'$currentuser'/Music'>>/tmp/file.lst
+echo '/home/'$currentuser'/Pictures'>>/tmp/file.lst
+echo '/home/'$currentuser'/Public'>>/tmp/file.lst
+echo '/home/'$currentuser'/Templates'>>/tmp/file.lst
+echo '/home/'$currentuser'/Videos'>>/tmp/file.lst
 
 ## Backup User Profile...
 echo -e "\033[1;33mUser \"$currentuser\"... \033[1;34mBackup User Profile\033[0m"
-tar --exclude-from=/tmp/file.lst -zcvf profile-backup.tar.gz --absolute-names '/home/'$currentuser'/'
+tar --exclude={'*.log','*.tmp'} --exclude-from=/tmp/file.lst -zcvf profile-backup.tar.gz --absolute-names '/home/'$currentuser'/'
 rm -f /tmp/file.lst
